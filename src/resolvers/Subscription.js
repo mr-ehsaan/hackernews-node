29837485ff7801eefabba7ff5c83962540eb3aa2
mr-ehsaan <m.ehsaan.shah1@gmail.com>
2023-04-26 18:26:12 +0500

    Revert "update subscription with space"
    This reverts commit 69e74b903137daba1c4c24cf6315cfe06fe238be.

--- src/resolvers/Subscription.js
+++ src/resolvers/Subscription.js
@@ -13,7 +13,6 @@ function newVoteSubscribe(parent, args, context, info) {
   return context.pubsub.asyncIterator("NEW_VOTE");
 }
 
-
 const newVote = {
   subscribe: newVoteSubscribe,
   resolve: (payload) => {
